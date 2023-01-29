9a9d2728f5a8f1e54dd6224845f5defff0cb2b5a
issach315 <issachemani227@gmial.com>
2023-01-29 05:50:09 +0530

    while loop in java

---
+++ WhileLoop.java
@@ -0,0 +1,22 @@
+package While_Loop;
+
+public class WhileLoop 
+{
+    public static void main(String[] args)
+     {
+
+        while (condition)
+        {
+         
+           /*
+            ---------------
+            ---------------
+            --operation's--
+            ---------------
+            ---------------
+           */
+        }
+        
+    }
+    
+}
